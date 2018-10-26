4d24c522575dd15ae9eb3b52bc0cda3d87ca4ea0
Conner Alegre <cga0@pm.me>
2018-10-26 09:21:48 -0600

    Add outlet and QuoteCore to Widget

--- ios-random-quote/QuoteWidget/TodayViewController.swift
+++ ios-random-quote/QuoteWidget/TodayViewController.swift
@@ -8,21 +8,18 @@
 
 import UIKit
 import NotificationCenter
+import QuoteCore
 
 class TodayViewController: UIViewController, NCWidgetProviding {
-        
+    
+    // MARK: - IBOutlets
+    @IBOutlet var quoteLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view from its nib.
     }
         
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
-        // Perform any setup necessary in order to update the view.
-        
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
-        
         completionHandler(NCUpdateResult.newData)
     }
     
