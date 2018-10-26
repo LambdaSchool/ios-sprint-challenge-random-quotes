55448a82cb4819e342b18c59f6a2b28b88fc1633
Conner Alegre <cga0@pm.me>
2018-10-26 09:39:27 -0600

    Add QuoteCoreWatch embedded binaries

---
+++ ios-random-quote/QuoteCore/QuoteCoreWatch/QuoteCoreWatch.h
@@ -0,0 +1,19 @@
+//
+//  QuoteCoreWatch.h
+//  QuoteCoreWatch
+//
+//  Created by Conner on 10/26/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for QuoteCoreWatch.
+FOUNDATION_EXPORT double QuoteCoreWatchVersionNumber;
+
+//! Project version string for QuoteCoreWatch.
+FOUNDATION_EXPORT const unsigned char QuoteCoreWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuoteCoreWatch/PublicHeader.h>
+
+
