1d08a87f0a33f1bb05b9dac54a6177c4fd7f9f92
stdemicheli <stdemicheli@gmail.com>
2018-10-26 17:14:56 +0200

    Initial commit. Added RandomQuotesCore framework.

---
+++ ios-random-quotes/Frameworks/ios-random-quotes-core/ios-random-quotes-core/Test.swift
@@ -0,0 +1,14 @@
+//
+//  Test.swift
+//  ios-random-quotes-core
+//
+//  Created by De MicheliStefano on 26.10.18.
+//  Copyright © 2018 De MicheliStefano. All rights reserved.
+//
+
+import Foundation
+
+public class Test {
+    
+    
+}
