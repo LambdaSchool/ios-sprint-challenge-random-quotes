46025ad0ff467b53a30c14833ef6819589c93b07
David O. Doswell <daviddoswell@lambda-school-loaner-11s-macbook-pro.local>
2018-10-26 09:11:21 -0700

    create model and model controller

---
+++ ios-sprint14-challenge/ios-sprint14-challenge/Model/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  ios-sprint14-challenge
+//
+//  Created by David Doswell on 10/26/18.
+//  Copyright © 2018 David Doswell. All rights reserved.
+//
+
+import Foundation
+
+struct Quote: Codable, Equatable {
+    var text: String
+}
