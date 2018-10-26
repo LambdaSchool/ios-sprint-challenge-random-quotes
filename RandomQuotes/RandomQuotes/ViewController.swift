49872c9ab5f80e9853c35419c765f4403467ee01
Linh Bouniol <paulalinh89@gmail.com>
2018-10-26 08:07:33 -0700

    Initial commit

---
+++ RandomQuotes/RandomQuotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomQuotes
+//
+//  Created by Linh Bouniol on 10/26/18.
+//  Copyright © 2018 Linh Bouniol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
