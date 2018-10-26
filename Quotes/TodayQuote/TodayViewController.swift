a279c57059f3d106042d9ab4ce654ad850ce85ca
Carolyn Lea <carolynrlea@gmail.com>
2018-10-26 12:35:12 -0600

    added watch

--- Quotes/TodayQuote/TodayViewController.swift
+++ Quotes/TodayQuote/TodayViewController.swift
@@ -11,21 +11,58 @@ import NotificationCenter
 
 class TodayViewController: UIViewController, NCWidgetProviding
 {
-        
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    
+    let quoteController = QuoteController()
+    var quote: Quote?
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        // Do any additional setup after loading the view from its nib.
+        self.extensionContext?.widgetLargestAvailableDisplayMode = .expanded
+        
+        quoteLabel.text = ""
+        authorLabel.text = ""
+        
+        quoteController.fetchQuote { (quote, error) in
+            
+            if let error = error {
+                NSLog("Error fetching: \(error)")
+                return
+            }
+            
+            guard let quote = quote else {
+                NSLog("No quote")
+                return
+            }
+            
+            self.quote = quote
+            
+            self.updateViews()
+        }
     }
         
-    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
-        // Perform any setup necessary in order to update the view.
-        
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
-        
-        completionHandler(NCUpdateResult.newData)
+    func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize)
+    {
+        switch activeDisplayMode {
+            case .compact:
+                preferredContentSize = maxSize
+                quoteLabel.numberOfLines = 1
+                authorLabel.isHidden = true
+            case .expanded:
+                preferredContentSize = CGSize(width: maxSize.width, height: 200)
+                quoteLabel.numberOfLines = 0
+                authorLabel.isHidden = false
+        }
     }
     
+    private func updateViews()
+    {
+        DispatchQueue.main.async {
+            
+            self.quoteLabel.text = self.quote?.quote ?? ""
+            self.authorLabel.text = self.quote?.author ?? ""
+        }
+    }
 }
