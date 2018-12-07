1f1ef6d608943628c9ce1d1bdfcbff0c074ede35
Daniela Parra <dyparra47@gmail.com>
2018-12-07 11:40:40 -0500

    Made framework from code base

---
+++ Frameworks/RandomQuoteCore/Quote.swift
@@ -0,0 +1,14 @@
+//
+//  Quote.swift
+//  RandomQuote
+//
+//  Created by Daniela Parra on 12/7/18.
+//  Copyright © 2018 Daniela Parra. All rights reserved.
+//
+
+import Foundation
+
+public struct RandomQuote: Codable {
+    public let quote: String
+    public let author: String
+}
