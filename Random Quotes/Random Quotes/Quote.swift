722dc4a5cfb384b25a7826dd62209fe01d8ef22d
Moin Uddin <U_Moin75@yahoo.com>
2018-12-07 11:25:06 -0500

    Create and Implement Quote Model and Quote Controller

---
+++ Random Quotes/Random Quotes/Quote.swift
@@ -0,0 +1,16 @@
+//
+//  Quote.swift
+//  Random Quotes
+//
+//  Created by Moin Uddin on 12/7/18.
+//  Copyright © 2018 Moin Uddin. All rights reserved.
+//
+
+import Foundation
+
+
+struct Quote: Codable {
+    let quote: String
+    let author: String
+    let category: String
+}
