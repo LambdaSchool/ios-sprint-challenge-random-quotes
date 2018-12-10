3dd159a1a822808b7195c2654614153f7183648e
Scott Bennett <stt.btt@gmail.com>
2018-12-10 12:53:35 -0500

    Resubmitted

--- RandomQuote/RandomQuoteWidget/TodayViewController.swift
+++ RandomQuote/RandomQuoteWidget/TodayViewController.swift
@@ -16,11 +16,22 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         
     @IBOutlet weak var quoteLabel: UILabel!
     @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var newQuoteLabel: UIButton!
     
     private let fetcher = QuoteFetcher()
     
+    private var quote: Quote? {
+        didSet {
+            DispatchQueue.main.async {
+                self.updateViews()
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.extensionContext?.widgetLargestAvailableDisplayMode = .expanded
         
     }
         
@@ -45,8 +56,48 @@ class TodayViewController: UIViewController, NCWidgetProviding {
             completionHandler(.newData)
         }
    
-        
         completionHandler(NCUpdateResult.newData)
     }
     
+    func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
+        switch activeDisplayMode {
+        case .compact:
+            preferredContentSize = maxSize
+            newQuoteLabel.isHidden = true
+        case .expanded:
+            updatePreferredContentSize()
+            newQuoteLabel.isHidden = false
+        }
+    }
+    
+    @IBAction func fetchNewQuote(_ sender: Any) {
+        fetchNewQuote()
+    }
+    
+    // MARK: - Private Functions
+    
+    private func fetchNewQuote() {
+        fetcher.fetchQuote { (quote, error) in
+            if let error = error {
+                NSLog("Error fetching quote: \(error)")
+                return
+            }
+            self.quote = quote
+        }
+    }
+    
+    private func updatePreferredContentSize() {
+        let size = quoteLabel.systemLayoutSizeFitting(quoteLabel.bounds.size)
+        preferredContentSize = CGSize(width: 0, height: size.height + 100)
+    }
+    
+    private func updateViews() {
+        guard let quote = quote else { return }
+        
+        DispatchQueue.main.async {
+            self.quoteLabel.text = quote.quote
+            self.authorLabel.text = "- \(quote.author)"
+            self.updatePreferredContentSize()
+        }
+    }
 }
