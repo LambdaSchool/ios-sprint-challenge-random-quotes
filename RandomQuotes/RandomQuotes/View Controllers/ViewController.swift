20fb3b938956c25218fcd9e0ff2cdccdca7a677e
Nelson Gonzalez <nelglezfl@gmail.com>
2019-04-12 07:32:51 -0400

    Set up Xcode with scenes and outlets/actions

---
+++ RandomQuotes/RandomQuotes/View Controllers/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  RandomQuotes
+//
+//  Created by Nelson Gonzalez on 4/12/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var quoteTextView: UITextView!
+    @IBOutlet weak var authorLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    @IBAction func fetchQuoteButton(_ sender: UIButton) {
+        
+        
+    }
+    
+}
+
