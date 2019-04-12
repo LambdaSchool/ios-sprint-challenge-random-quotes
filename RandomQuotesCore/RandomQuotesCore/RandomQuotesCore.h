0686333024c6311e2dbef626cc5f7e5f5d7a0bbb
Moses Robinson <mosesjrobinson@icloud.com>
2019-04-12 09:29:24 -0700

    framework core added

---
+++ RandomQuotesCore/RandomQuotesCore/RandomQuotesCore.h
@@ -0,0 +1,19 @@
+//
+//  RandomQuotesCore.h
+//  RandomQuotesCore
+//
+//  Created by Moses Robinson on 4/12/19.
+//  Copyright © 2019 Moses Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RandomQuotesCore.
+FOUNDATION_EXPORT double RandomQuotesCoreVersionNumber;
+
+//! Project version string for RandomQuotesCore.
+FOUNDATION_EXPORT const unsigned char RandomQuotesCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RandomQuotesCore/PublicHeader.h>
+
+
