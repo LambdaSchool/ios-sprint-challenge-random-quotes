742ee16643f1e3b72275461b6a9929b84501aa3e
Moses Robinson <mosesjrobinson@icloud.com>
2019-04-12 12:38:51 -0700

    trying to import coreframework

---
+++ RandomQuotesCore/RandomQuotesCore-watchOS/RandomQuotesCore_watchOS.h
@@ -0,0 +1,19 @@
+//
+//  RandomQuotesCore_watchOS.h
+//  RandomQuotesCore-watchOS
+//
+//  Created by Moses Robinson on 4/12/19.
+//  Copyright © 2019 Moses Robinson. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for RandomQuotesCore_watchOS.
+FOUNDATION_EXPORT double RandomQuotesCore_watchOSVersionNumber;
+
+//! Project version string for RandomQuotesCore_watchOS.
+FOUNDATION_EXPORT const unsigned char RandomQuotesCore_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RandomQuotesCore_watchOS/PublicHeader.h>
+
+
