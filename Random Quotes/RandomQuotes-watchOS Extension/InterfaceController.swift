83ea37dd32a8b8a01b97f86a3bc1852750e3f9de
Moses Robinson <mosesjrobinson@icloud.com>
2019-04-12 13:35:30 -0700

    fetches when watch wakes up

--- Random Quotes/RandomQuotes-watchOS Extension/InterfaceController.swift
+++ Random Quotes/RandomQuotes-watchOS Extension/InterfaceController.swift
@@ -14,7 +14,9 @@ class InterfaceController: WKInterfaceController {
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
+    }
+    
+    override func didAppear() {
         quoteController.fetchRandomQuote { (quote, error) in
             
             if let error = error {
