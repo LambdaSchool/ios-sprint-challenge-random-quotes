ddbe896c35924429f723f47468fd73dcbce2a75e
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-04-19 12:57:18 -0400

    add today and watchOS extension

--- RandomQuotes/RandomQuotes/QuoteController.swift
+++ RandomQuotes/RandomQuotes/QuoteController.swift
@@ -7,3 +7,69 @@
 //
 
 import Foundation
+import UIKit
+
+class QuoteController {
+    
+    func getRandomQuote(_ randomQuote: String, completion: @escaping (Quote?, Error?) -> Void) {
+        
+        var request = URLRequest(url: baseURL)
+        
+        request.setValue(apiKey, forHTTPHeaderField: "X-Mashape-Key")
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            
+            
+            if let error = error {
+                NSLog("Error fetching quote: \(error)")
+                completion(nil, error)
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            
+            do {
+                quote = try JSONDecoder().decode([Quote.self], from: data)
+            } catch {
+                NSLog("Error decoding Quote: \(error)")
+            }
+            
+            completion(self.quote, nil)
+            
+            
+        }.resume()
+
+
+
+
+
+    }
+    
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    // MARK: - Properties
+    
+    private let baseURL = URL(string: "https://andruxnet-random-famous-quotes.p.rapidapi.com/")!
+    private let apiKey = "61294c2c10msh897c8dc53e129eep1399b9jsnf3153a2bbd0c"
+    var quote: Quote?
+}
