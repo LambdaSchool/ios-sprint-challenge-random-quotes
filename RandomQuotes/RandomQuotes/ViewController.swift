74657e46381f118b5aea7e5ecd0d8079dcb0996d
Frulwinn <frulwinn@gmail.com>
2019-04-19 10:09:04 -0500

    initial commit

---
+++ RandomQuotes/RandomQuotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomQuotes
+//
+//  Created by Lambda_School_Loaner_34 on 4/19/19.
+//  Copyright © 2019 Frulwinn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
