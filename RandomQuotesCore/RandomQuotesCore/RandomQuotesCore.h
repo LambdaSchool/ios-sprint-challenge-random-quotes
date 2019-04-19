dc27fb3ccbd5b2fa1adf3099c484f3ec7b41ebcd
Frulwinn <frulwinn@gmail.com>
2019-04-19 10:26:43 -0500

    made a core framework and added it to RandomQuotes Project

---
+++ RandomQuotesCore/RandomQuotesCore/RandomQuotesCore.h
@@ -0,0 +1,19 @@
+//
+//  RandomQuotesCore.h
+//  RandomQuotesCore
+//
+//  Created by Lambda_School_Loaner_34 on 4/19/19.
+//  Copyright © 2019 Frulwinn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RandomQuotesCore.
+FOUNDATION_EXPORT double RandomQuotesCoreVersionNumber;
+
+//! Project version string for RandomQuotesCore.
+FOUNDATION_EXPORT const unsigned char RandomQuotesCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RandomQuotesCore/PublicHeader.h>
+
+
