ddf513f906179d0f2c441b6784ce48b5d5375c9a
Frulwinn <frulwinn@gmail.com>
2019-04-19 10:32:23 -0500

    added Quote model to Random Quotes Core

---
+++ RandomQuotesCore/RandomQuotesCore/Quote.swift
@@ -0,0 +1,14 @@
+//
+//  Quote.swift
+//  RandomQuotesCore
+//
+//  Created by Lambda_School_Loaner_34 on 4/19/19.
+//  Copyright © 2019 Frulwinn. All rights reserved.
+//
+
+import Foundation
+
+struct Quote: Codable {
+    let quote: String
+    let author: String
+}
---
+++ RandomQuotesCore/RandomQuotesCore/QuoteController.swift
@@ -0,0 +1,9 @@
+//
+//  QuoteController.swift
+//  RandomQuotesCore
+//
+//  Created by Lambda_School_Loaner_34 on 4/19/19.
+//  Copyright © 2019 Frulwinn. All rights reserved.
+//
+
+import Foundation
