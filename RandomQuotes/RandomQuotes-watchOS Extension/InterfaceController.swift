882fc806ad8ecd3359f7524be59f6fae369f3f36
nateyoungren <nathanaelyoungren@gmail.com>
2019-04-19 08:43:28 -0700

    Add watchOS app

---
+++ RandomQuotes/RandomQuotes-watchOS Extension/InterfaceController.swift
@@ -0,0 +1,46 @@
+//
+//  InterfaceController.swift
+//  RandomQuotes-watchOS Extension
+//
+//  Created by Nathanael Youngren on 4/19/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        quoteController.fetchQuote { (quote, error) in
+            if let error = error {
+                print("Error running fetchQuote method: \(error).")
+                return
+            }
+            self.quote = quote
+        }
+    }
+    
+    func updateViews() {
+        if let quote = quote {
+            quoteLabel.setText(quote.quote)
+            let author = "- " + quote.author
+            authorLabel.setText(author)
+        }
+    }
+    
+    var quote: Quote? {
+        didSet {
+            DispatchQueue.main.async {
+                self.updateViews()
+            }
+        }
+    }
+    
+    @IBOutlet weak var quoteLabel: WKInterfaceLabel!
+    @IBOutlet weak var authorLabel: WKInterfaceLabel!
+    
+    let quoteController = QuoteController()
+}
