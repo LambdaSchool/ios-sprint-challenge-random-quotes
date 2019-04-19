18328335e83e60c934368961019fce3ea6fade15
Frulwinn <frulwinn@gmail.com>
2019-04-19 11:37:06 -0500

    added watchOS and extension

---
+++ RandomQuotes/RandomQuotes-watchOS Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  RandomQuotes-watchOS Extension
+//
+//  Created by Lambda_School_Loaner_34 on 4/19/19.
+//  Copyright © 2019 Frulwinn. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
