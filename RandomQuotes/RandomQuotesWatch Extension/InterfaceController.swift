ccf9c456a1e144102e7451c8dfead8029d0507e4
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-04-19 13:21:47 -0400

    set everything up

--- RandomQuotes/RandomQuotesWatch Extension/InterfaceController.swift
+++ RandomQuotes/RandomQuotesWatch Extension/InterfaceController.swift
@@ -10,6 +10,7 @@ import WatchKit
 import Foundation
 
 
+
 class InterfaceController: WKInterfaceController {
 
     override func awake(withContext context: Any?) {
@@ -28,4 +29,7 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+    
+    // MARK: - Properties
+    
 }
